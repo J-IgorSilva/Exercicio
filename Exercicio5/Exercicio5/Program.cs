9a03e4c425ad62593f591c5d62fc452c854fcf04
J-IgorSilva <aleatoria8821@gmail.com>
2022-09-22 14:08:45 -0300

    Exercicos Programação

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros Antecessor e Sucessor\n");
+            Console.WriteLine("Digite um Numero\n");
+            int numero;
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("O antecessor de "+ (numero-1)+ " e o sucesso e "+(numero+1));
+
+        }
+        
+    }
+}
---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero\n");
+            float numero = float.Parse(Console.ReadLine());
+            Console.WriteLine("O dobro desse numero é "+(numero*2)+" e sua terça parte é "+(numero/3));
+        }
+    }
+}
