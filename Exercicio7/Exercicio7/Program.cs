ad8156446abf70a45ed8e07de1adec6134b48563
J-IgorSilva <aleatoria8821@gmail.com>
2022-09-28 09:36:08 -0300

    Exercicios de Sequencias Basicas

---
+++ Exercicio7/Exercicio7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de medidas\n");
+            float numero = float.Parse(Console.ReadLine());
+            Console.WriteLine("A distancia em metros é "+ (numero/1000));
+            Console.WriteLine("A distancia em centimetros é " + (numero * 1000));
+
+        }
+    }
+}
---
+++ Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quando Dinheiro Possui na carteira");
+
+            double money = double.Parse(Console.ReadLine());
+            var dollar = 5.21;
+
+            var price = money / dollar;
+
+            Console.WriteLine($"Na conversão você Possui {price} dolares");
+
+            //string[] numeros = Console.ReadLine().Split(' ');
+            //Console.WriteLine("digite nomes");
+            //string[] Linha = Console.ReadLine().Split(' ');
+
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    Console.WriteLine($"este é seu{numeros[i]}");
+            //}
+        }    
+    }
+}
