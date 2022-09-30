eee5453dcadf32dccde0700abcfa7e20a26c8315
J-IgorSilva <aleatoria8821@gmail.com>
2022-09-30 09:02:12 -0300

    Exercicios de sequencias Basicas

---
+++ Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 valores para calcular o valor de Delta!");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int delta ,x ;
+
+            delta =(b * b )- 4*a*c;
+
+            x = (int)(-b + (Math.Sqrt(b) - 4 * a * c) / 2*a);
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ Exercicio9/Exercicio9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanto de largura a tera a area pintada?");
+
+            double heigth = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quanto de altura tem sua parede?");
+
+            double width = double.Parse(Console.ReadLine());
+
+            var area = width * heigth;
+            var painting = (width * heigth) *2;
+
+            Console.WriteLine($"Voce Gastara {painting} litro de tinto para pintar uma parede de {area} metros");
+        }
+    }
+}
