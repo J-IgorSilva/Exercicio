019de0cac63032acea3e807ad990c621a233bafe
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-06 20:14:28 -0300

    Sequencias Basicas

---
+++ Exercicio11/Exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual preço do produto?");
+            var produto = int.Parse(Console.ReadLine());
+            var desconto = (produto * 5) / 100;
+
+            Console.WriteLine($"O produto custa { produto}, Com desconto o preço final é {produto-desconto}");
+        }
+    }
+}
