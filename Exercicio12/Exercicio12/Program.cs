920116b08cfbffb916479e336068c011f0b1d4c6
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-06 20:14:54 -0300

    Sequencias Basicas Calculo

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite o valor do salario");
+            double funcionarioSalario = double.Parse(Console.ReadLine());
+            var aumentoSalario = (funcionarioSalario * 15) / 100;
+
+            Console.WriteLine($"O valor do salario do funcionario é de {funcionarioSalario}\n");
+            Console.WriteLine($"Com o aumento vigente passara a ser de {aumentoSalario+funcionarioSalario}");
+
+        }
+    }
+}
