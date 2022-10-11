b78ef8b055face6d55710b48887548315cfbf303
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-11 09:06:05 -0300

    Sequencias Basicas[

---
+++ Exercicio14/Exercicio14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos dias o funcionario trabalhou esse mes?");
+            int diasTrabalhado = int.Parse(Console.ReadLine());
+            var hora = 25;
+
+            Console.WriteLine($"valor a ser pago para o funcionario é de :{(diasTrabalhado*8)*hora}");
+
+
+        }
+    }
+}
