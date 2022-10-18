70d2c9a7f823e2e96e9be58d7855e9bddf97ebbc
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-18 09:39:54 -0300

    Sequencias Basicas

---
+++ Exercicio15/Exercicio15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hora 1440 minutos
+            //ano 8760 horas
+            // dia possue 34560 minutos
+
+            Console.WriteLine("Quantos Cigarros voçê fuma por dia?");
+            int cigarros = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos anos voçê ja fumou?");
+            int anos = int.Parse(Console.ReadLine());
+
+            var dias = (anos * 365);
+
+            var tempoPerdido = (dias * 34560) / (cigarros * 10);
+            Console.WriteLine($"Durante {anos}Anos, fumando voçê ja perdeu {tempoPerdido} minutos");
+
+        }
+    }
+}
