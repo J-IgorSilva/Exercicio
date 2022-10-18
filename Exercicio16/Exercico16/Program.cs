8eb86d2c599bb3a59c353e60bf8ab62c42af13e9
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-18 09:41:09 -0300

    Desafio Sequencias Basicas

---
+++ Exercicio16/Exercico16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercico16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a velocidade atual do carro?");
+            int velocidade = int.Parse(Console.ReadLine());
+
+            if (velocidade > 80)
+            {
+                Console.WriteLine($"Voçê excedeu a velocidade permitida, sua multa atual é {(velocidade-80)*5}");
+            }
+            else
+            {
+                Console.WriteLine("Você não possue multas ate o momento:");
+            }
+        }
+    }
+}
