e254f84fc8a95981cfead05bb361b71e705d8c92
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-19 18:15:20 -0300

    Condições Basicas

---
+++ Exercicio18/Exercicio18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio18
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            var aluno = Console.ReadLine();
+            double media, nota1, nota2;
+            Console.WriteLine("Qual a primeira nota do aluno? ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a Segunda Nota: ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            media = nota1 + nota2 / 2;
+
+            if (media < 7)
+            {
+                Console.WriteLine($"A media final do aluno {aluno} é: {media}, REPROVADO");
+            }
+            else
+            {
+                Console.WriteLine($"A media do aluno {aluno} é: {media}, APROVADO");
+            }
+
+           
+        }
+    }
+}
