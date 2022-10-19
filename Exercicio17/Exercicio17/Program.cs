99850c3f0ba136b4839cd6ba43d7d108ea3ee3ba
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-19 18:14:18 -0300

    Condições Basicas

---
+++ Exercicio17/Exercicio17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Em que ano voçê nasceu ?");
+            int anoDeNascimento = int.Parse(Console.ReadLine());
+            var idadeAtual = 2022-anoDeNascimento;
+
+            if (idadeAtual < 18)
+            {
+                Console.WriteLine($"Voçê possue atualmente {idadeAtual}Anos, seu voto não é obrigatorio:");
+            }
+            else
+            {
+                Console.WriteLine($"Você Possue {idadeAtual}Anos, Seu voto é obrigatorio.");
+            }
+            
+        }
+    }
+}
