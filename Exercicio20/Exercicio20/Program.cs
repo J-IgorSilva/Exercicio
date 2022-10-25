feaf002e89a044a1409fb1cad7a0c8bafbd95dd1
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-25 08:53:08 -0300

    Sequecias Basicas

---
+++ Exercicio20/Exercicio20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Descrubra se o ano é bissexto, Digite um ano: ");
+
+            int ano = int.Parse(Console.ReadLine());
+
+            if ((ano % 400 ==0)&&(ano%100!=0))
+            {
+                Console.WriteLine($"{ano} é um ano BISSEXTO");
+            }
+            else
+            {
+                Console.WriteLine("Esse ano não é BISSEXTO");
+                
+            }
+        }
+    }
+}
